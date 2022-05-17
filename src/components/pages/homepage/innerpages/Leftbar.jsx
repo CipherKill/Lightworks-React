f2d3f39acdcf5c8a0886ae6d567d66c4d231078f
Alan Raju <anonymousisactive@gmail.com>
2022-05-17 09:40:35 +0530

    fixed css conficts

--- src/components/pages/homepage/innerpages/Leftbar.jsx
+++ src/components/pages/homepage/innerpages/Leftbar.jsx
@@ -13,8 +13,8 @@ function Leftbar(){
     <Container fluid className='offset-margin'>
       <h1 className='heading'>Welcome to Lightworks</h1>
       <p className='description'><span className='format-quote'>&quot;To invent, you need a good imagination and a pile of junk.&quot; </span>- Thomas Edision</p>
-      <button type='button' className='btn'><Link to='/bulbs' className='link-format'>See Bulbs</Link></button>
-      <button type='button' onClick={switchBulb} className='btn link-format'>Click to see magic</button>
+      <button type='button' className='btnn'><Link to='/bulbs' className='link-format'>See Bulbs</Link></button>
+      <button type='button' onClick={switchBulb} className='btnn link-format'>Click to see magic</button>
     </Container>
   );
 }
